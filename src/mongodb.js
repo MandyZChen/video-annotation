1b54d46437f7b9e25a3b26df81ad9ad450be00c0
mandychan101 <mandychan@pku.edu.cn>
2017-08-20 16:42:50 -0700

    Add production mongodb url

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,6 +4,6 @@ module.exports = function () {
   const app = this;
   const config = app.get('mongodb');
   const promise = MongoClient.connect(config);
-
+  console.log('Connecting to MongoDb on ' + config);
   app.set('mongoClient', promise);
 };
